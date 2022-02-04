5409286ffb5a708ee5a978ffe17a8f9d606301f1
SF-CPU-251\saurav <saurav.b@simformsolutions.com>
2022-02-04 16:55:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DebuggingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userResponse = Console.ReadLine();
+            if(int.TryParse(userResponse,out int value))
+            {
+                Console.WriteLine("The entered value is an integer");
+            }
+            else
+            {
+                Console.WriteLine("The entered value is not an integer");
+            }
+        }
+    }
+}
